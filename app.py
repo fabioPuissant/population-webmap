c2e759ec831ff21eff6169995f7b734a2b57ca21
FabioP <fabio.puissant@hotmail.com>
2020-09-07 18:18:18 +0200

    Added layers so a user can choose which data he/she wants to visualize on the map!

--- app.py
+++ app.py
@@ -11,8 +11,9 @@ display_map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen T
 html = "<h4>Volcano information:</h4>" \
        "<b>Location</b>: <i>%s</i><br/>" \
        "<b>Height</b>: <i>%s<i/> <b>m</b>"
-fg = folium.FeatureGroup(name="My Map")
 
+# Making the volcanoes display layer on the world map!
+fg_volcanoes = folium.FeatureGroup(name="Volcanoes")
 for (lat, lon, name, elevation) in zip(
         list(volcanoes_data["LAT"]),
         list(volcanoes_data["LON"]),
@@ -20,7 +21,7 @@ for (lat, lon, name, elevation) in zip(
         list(volcanoes_data["ELEV"])
 ):
     iframe = folium.IFrame(html % (name, elevation), width=200, height=100)
-    fg.add_child(
+    fg_volcanoes.add_child(
         folium.CircleMarker(
             location=(lat, lon),
             popup=folium.Popup(iframe, parse_html=True),
@@ -31,8 +32,10 @@ for (lat, lon, name, elevation) in zip(
         )
     )
 
-# Add polygon Layer
-fg.add_child(
+# Adding population Layer to the world map
+fg_population = folium.FeatureGroup(name="Population")
+
+fg_population.add_child(
     folium.GeoJson(
         data=open("world.json", "r", encoding="utf-8-sig").read(),
         style_function=lambda x: {'fillColor':
@@ -43,5 +46,9 @@ fg.add_child(
     )
 )
 
-display_map.add_child(fg)
+#Important to first add a feature group before adding Layer control!!
+display_map.add_child(fg_volcanoes)
+display_map.add_child(fg_population)
+display_map.add_child(folium.LayerControl()) #Now the layer control will be added to the map
+
 display_map.save("Map1.html")
