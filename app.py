9ea32ccd1fa220982a580fe26b6d4ee448aa9aa8
Fabio_Puissant <fabio.puissant@hotmail.com>
2020-09-07 13:48:48 +0200

    Make custom marker for the vulcano map

--- app.py
+++ app.py
@@ -1,24 +1,34 @@
 import folium
 import pandas
 
+
+def color_producer(elv):
+    return 'red' if elv >= 3000 else 'orange' if elv >= 1000 else 'green'
+
+
 volcanoes_data = pandas.read_csv("volcanoes.csv")
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 html = "<h4>Volcano information:</h4>" \
-       "<b>Name</b>: <i>%s</i><br/>" \
+       "<b>Location</b>: <i>%s</i><br/>" \
        "<b>Height</b>: <i>%s<i/> <b>m</b>"
 fg = folium.FeatureGroup(name="My Map")
+
 for (lat, lon, name, elevation) in zip(
         list(volcanoes_data["LAT"]),
         list(volcanoes_data["LON"]),
         list(volcanoes_data["NAME"]),
         list(volcanoes_data["ELEV"])
 ):
-    coordinates = [lat, lon]
     iframe = folium.IFrame(html % (name, elevation), width=200, height=100)
-    fg.add_child(folium.Marker(
-        location=coordinates,
-        popup= folium.Popup(iframe, parse_html=True),
-        icon=folium.Icon(color="green"))
+    fg.add_child(
+        folium.CircleMarker(
+            location=(lat, lon),
+            popup=folium.Popup(iframe, parse_html=True),
+            radius=8,
+            fill_color=color_producer(elevation),
+            color="grey",
+            fill_opacity=0.7
+        )
     )
 
 map.add_child(fg)
